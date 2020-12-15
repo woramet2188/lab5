8f30d909387787b94d033e460a5c39651f5444dc
woramet2188 <ballhello678@gmail.com>
2020-12-15 17:34:30 +0700

    lab5_2.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
-
+int i=1,num,x=-1,y=0;
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    while(i!=0){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num%2==0){
+            x++;
+        }
+        else{
+            y++;
+        }
+        i=num;
+    }
+    cout << "#Even numbers = "<<x;
+    cout << "\n#Odd numbers = "<<y;
     return 0;
 }
--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,7 +1,33 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
-
+#include<cmath>
 using namespace std;
-
+double deg2rad(double i){
+    double j=(i*M_PI)/180;
+    return j;
+}
+double rad2deg(double j){
+    double i=(j*180)/M_PI;
+    return i;
+}
+double findXComponent(double x1,double x2,double x3,double x4){
+    double x=x1*cos(x3)+x2*cos(x4);
+    return x;
+}
+double findYComponent(double y1,double y2,double y3,double y4){
+    double y=y1*sin(y3)+y2*sin(y4);
+    return y;
+}
+double pythagoras(double X,double Y){
+    double Rvl=sqrt(pow(X,2)+pow(Y,2));
+    return Rvl;
+}
+void showResult(double rvl,double rvd){
+    cout <<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+    cout <<"\nLength of the resultant vector = "<< rvl;
+    cout <<"\nDirection of the resultant vector (deg) = "<< rvd;
+    cout <<"\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
